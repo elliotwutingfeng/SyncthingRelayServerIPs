3e6089aa175a336cec33602c08ca39490f0ccced
Tingfeng <wu.tingfeng@u.nus.edu>
2022-04-01 16:05:50 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Syncthing Relay Server IP Addresses
+
+![Rust](https://img.shields.io/badge/Rust-000000?style=for-the-badge&logo=rust&logoColor=white)
+![GitHub Actions](https://img.shields.io/badge/GitHub_Actions-2088FF?style=for-the-badge&logo=github-actions&logoColor=white)
+
+[![GitHub license](https://img.shields.io/badge/LICENSE-BSD--3--CLAUSE-GREEN?style=for-the-badge)](LICENSE)
+[![scraper](https://img.shields.io/github/workflow/status/elliotwutingfeng/SyncthingRelayServerIPs/scraper?label=SCRAPER&style=for-the-badge)](https://github.com/elliotwutingfeng/SyncthingRelayServerIPs/actions/workflows/scraper.yml)
+<img src="https://img.shields.io/tokei/lines/github/elliotwutingfeng/SyncthingRelayServerIPs?label=Total%20Allowlist%20IPs&style=for-the-badge" alt="Total Allowlist IPs"/>
+
+Machine-readable `.txt` allowlist of [Syncthing Relay Server IP Addresses](https://relays.syncthing.net), updated every 5 minutes.
+
+**Disclaimer:** _This project is not sponsored, endorsed, or otherwise affiliated with Syncthing._
+
+## Allowlist download
+You may download the allowlist [here](ips.txt?raw=1)
+
+## Requirements
+
+-   Rust >= 1.59.0
+
+## Setup instructions
+
+`git clone` and `cd` into the project directory, then run the following
+
+```bash
+cargo build
+```
+
+## Usage
+
+```bash
+cargo run
+```
+
+&nbsp;
+
+<sup>These files are provided "AS IS", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders be liable for any claim, damages or other liability, arising from, out of or in connection with the files or the use of the files.</sup>
+
+<sub>Any and all trademarks are the property of their respective owners.</sub>
---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use reqwest;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::net::Ipv4Addr;
+
+#[allow(dead_code)]
+#[tokio::main]
+async fn main() {
+    let url = format!("https://relays.syncthing.net/endpoint");
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(url)
+        .header("CONTENT_TYPE", "application/json")
+        .header("ACCEPT", "application/json")
+        .send()
+        .await
+        .unwrap();
+    
+    match response.status() {
+        reqwest::StatusCode::OK => {
+            match response.json::<APIResponse>().await {
+                Ok(parsed) => {
+                    let mut ipv4_list: Vec<Ipv4Addr> = Vec::new();
+                    for relay in parsed.relays {
+                        let ip_address: Ipv4Addr = relay.url
+                        .split("relay://") // remove "relay://" prefix
+                        .collect::<Vec<_>>()[1]
+                        .split(":") // remove port number suffix
+                        .collect::<Vec<_>>()[0]
+                        .parse()
+                        .expect("Unable to parse ipv4 address");
+
+                        ipv4_list.push(ip_address);
+                    }
+
+                    ipv4_list.sort();
+
+                    let ipv4_string: String = ipv4_list
+                    .into_iter()
+                    .map(|ip| ip.to_string())
+                    .collect::<Vec<_>>()
+                    .join("\n");
+
+                    if ipv4_string.chars().count() != 0 {
+                    let path = Path::new("ips.txt");
+                    let mut file = match File::create(&path) {
+                        Err(why) => panic!("couldn't create file: {}", why),
+                        Ok(file) => file,
+                    };
+
+                    match file.write_all(ipv4_string.as_bytes()) {
+                        Err(why) => panic!("couldn't write to file: {}", why),
+                        Ok(_) => println!("successfully wrote to file"),
+                    }
+                }
+                },
+                Err(_) => println!("Bad response shape"),
+            };
+        }
+        other => {
+            panic!("GET request failed: {:?}", other);
+        }
+    };
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Relay {url: String}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct APIResponse {relays: Vec<Relay>}
